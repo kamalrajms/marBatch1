150cfd4c18ebafe69779ba5e7d527b3a028c955b
kamalrajms <kamalrajindtn@gmail.com>
2026-03-02 13:39:35 +0530

    Intro

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Greeting from './Greeting'
+
+export default function App() {
+  return (
+    <div>
+      <Greeting/>
+      <h2>hello</h2>
+    </div>
+  )
+}
---
+++ src/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hello from "./Hello";
+
+export default function Greeting() {
+  return (
+    <div>
+      <h2>Greeting hello every one</h2>
+      <Hello />
+    </div>
+  );
+}
